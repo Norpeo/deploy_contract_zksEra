5b2b398aa9593fe4c63fd5ea3f691bfb22a17b89
Norpeo <6207084@qq.com>
2023-05-24 17:00:14 +0800

    fix yarn package install bug

--- ts/Ryarn.bat
+++ ts/Ryarn.bat
@@ -2,7 +2,9 @@ if exist "yarn.lock" (
   goto :success
 )
 
-yarn install
+yarn init -y
+yarn add -D typescript ts-node ethers@^5.7.2 zksync-web3 hardhat @matterlabs/hardhat-zksync-solc @matterlabs/hardhat-zksync-deploy
+
 
 :success
 
